b5908ddb783b3d77078e2b4ce99806d1cddb0c48
omk18p <omkar003628p@gmail.com>
2025-01-19 12:02:00 +0530

    ci: use latest node version

--- .activity.log.js
+++ .activity.log.js
@@ -87,3 +87,4 @@
 // commit 1 on 2025-01-21: fix: crash on login edge case
 // commit 2 on 2025-01-21: ci: fix deploy script
 // commit 1 on 2025-01-19: style: center align text
+// commit 2 on 2025-01-19: ci: use latest node version
