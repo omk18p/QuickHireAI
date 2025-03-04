af0056df1647fab63163c002f1a31d6ee80b9072
omk18p <omkar003628p@gmail.com>
2025-03-04 12:01:00 +0530

    update: reorder navbar items

--- .activity.log.js
+++ .activity.log.js
@@ -62,3 +62,4 @@
 // commit 1 on 2025-03-21: refactor: cleanup function naming
 // commit 1 on 2025-03-20: style: center align text
 // commit 1 on 2025-03-18: docs: add API documentation
+// commit 1 on 2025-03-04: update: reorder navbar items
