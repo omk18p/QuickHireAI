0cb188e593b9d66bb240551e30ed3b3558ebb67d
omk18p <omkar003628p@gmail.com>
2025-05-28 12:01:00 +0530

    refactor: extract utility method

--- .activity.log.js
+++ .activity.log.js
@@ -21,3 +21,4 @@
 // commit 1 on 2025-06-01: style: responsive layout tweaks
 // commit 1 on 2025-05-29: test: snapshot test update
 // commit 2 on 2025-05-29: feat: add dark mode toggle
+// commit 1 on 2025-05-28: refactor: extract utility method
