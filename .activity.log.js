bf0ff56393838a4ed9587c280fcec563a55415dc
omk18p <omkar003628p@gmail.com>
2025-07-02 12:01:00 +0530

    refactor: move logic to hooks

--- .activity.log.js
+++ .activity.log.js
@@ -4,3 +4,4 @@
 // commit 1 on 2025-07-07: chore: delete unused file
 // commit 2 on 2025-07-07: docs: improve explanation in overview
 // commit 1 on 2025-07-05: docs: improve explanation in overview
+// commit 1 on 2025-07-02: refactor: move logic to hooks
