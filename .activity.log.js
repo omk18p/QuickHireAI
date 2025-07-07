edf39fc544023e1e4202c6aa54bd0fc3b7dfa289
omk18p <omkar003628p@gmail.com>
2025-07-07 12:02:00 +0530

    docs: improve explanation in overview

--- .activity.log.js
+++ .activity.log.js
@@ -2,3 +2,4 @@
 // commit 1 on 2025-07-08: style: responsive layout tweaks
 // commit 2 on 2025-07-08: feat: improve error message UX
 // commit 1 on 2025-07-07: chore: delete unused file
+// commit 2 on 2025-07-07: docs: improve explanation in overview
