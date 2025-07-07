121b1074cac19f35250c3200c4e05e98e151196d
omk18p <omkar003628p@gmail.com>
2025-07-07 12:01:00 +0530

    chore: delete unused file

--- .activity.log.js
+++ .activity.log.js
@@ -1,3 +1,4 @@
 // commit 1 on 2025-07-10: style: responsive layout tweaks
 // commit 1 on 2025-07-08: style: responsive layout tweaks
 // commit 2 on 2025-07-08: feat: improve error message UX
+// commit 1 on 2025-07-07: chore: delete unused file
